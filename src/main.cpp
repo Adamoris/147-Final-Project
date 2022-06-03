0c99d65a546888a17b462b4c6cb65251f149fa64
Andrew Park <apark5@uci.edu>
2022-06-03 14:46:31 -0700

    SPO2 Added
    Co-authored-by: Nicholas Dinh <nicholasdinh@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <MAX30105.h>
 #include <heartRate.h>
 #include <spo2_algorithm.h>
+#include <Pulse.h>
 #include <string>
 #include <sstream>
 #include <TFT_eSPI.h>
@@ -19,6 +20,7 @@
 #define DHT11_PIN 13
 #define PIEZO_IN 36
 #define PIEZO_OUT 37
+#define DEHUMIDIFIER_OUT 2
 #define DHTTYPE DHT11
 
 // Initialized Variables
@@ -27,6 +29,18 @@ DHT dht(DHT11_PIN, DHT11);
 
 LSM6DSO myIMU; //Default constructor is I2C, addr 0x6B
 MAX30105 particleSensor;
+Pulse pulseIR;
+Pulse pulseRed;
+float SPO2;
+MAFilter bpm;
+// uint32_t irBuffer[100]; //infrared LED sensor data
+// uint32_t redBuffer[100];  //red LED sensor data
+// int32_t bufferLength; //data length
+// int32_t spo2; //SPO2 value
+// int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+// int32_t heartRate; //heart rate value
+// int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
 const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
@@ -117,6 +131,7 @@ void setup() {
   Serial.println(WiFi.macAddress());
 
   pinMode(PIEZO_OUT, OUTPUT);
+  pinMode(DEHUMIDIFIER_OUT, OUTPUT);
   delay(500); 
 
   Wire.begin();
@@ -141,6 +156,16 @@ void setup() {
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
   particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 
+  // byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  // byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  // byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  // byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  // int pulseWidth = 411; //Options: 69, 118, 215, 411
+  // int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  // particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+
   dht.begin();
   delay(1000);
 
@@ -150,189 +175,300 @@ void setup() {
 }
 
 void loop() {
-  int err =0;
-  
-  WiFiClient c;
-  HttpClient http(c);
+  // bufferLength = 100;
 
-  // Get sensor readings
-  reading = analogRead(PIEZO_IN);
-  float h = dht.readHumidity();
-  delay(10);
-  float t = dht.readTemperature();
+  // for (byte i = 0 ; i < bufferLength ; i++)
+  // {
+  //   while (particleSensor.available() == false) //do we have new data?
+  //     particleSensor.check(); //Check the sensor for new data
 
-  long irValue = particleSensor.getIR();
+  //   redBuffer[i] = particleSensor.getRed();
+  //   irBuffer[i] = particleSensor.getIR();
+  //   particleSensor.nextSample(); //We're finished with this sample so move to next sample
 
-  if (checkForBeat(irValue) == true)
-  {
-    //We sensed a beat!
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
+  //   // Serial.print(F("red="));
+  //   // Serial.print(redBuffer[i], DEC);
+  //   // Serial.print(F(", ir="));
+  //   // Serial.println(irBuffer[i], DEC);
+  // }
 
-    beatsPerMinute = 60 / (delta / 1000.0);
+  // //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  // maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+  // while(1)
+  // {
+  //   //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+  //   for (byte i = 25; i < 100; i++)
+  //   {
+  //     redBuffer[i - 25] = redBuffer[i];
+  //     irBuffer[i - 25] = irBuffer[i];
+  //   }
 
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
+  //   //take 25 sets of samples before calculating the heart rate.
+  //   for (byte i = 75; i < 100; i++)
+  //   {
+  //     while (particleSensor.available() == false) //do we have new data?
+  //       particleSensor.check(); //Check the sensor for new data
+
+  //     redBuffer[i] = particleSensor.getRed();
+  //     irBuffer[i] = particleSensor.getIR();
+  //     particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+  //     //send samples and calculation result to terminal program through UART
+  //     // Serial.print(F("red="));
+  //     // Serial.print(redBuffer[i], DEC);
+  //     // Serial.print(F(", ir="));
+  //     // Serial.print(irBuffer[i], DEC);
+
+  //     // Serial.print(F(", HR="));
+  //     // Serial.print(heartRate, DEC);
+
+  //     // Serial.print(F(", HRvalid="));
+  //     // Serial.print(validHeartRate, DEC);
+
+  //     // Serial.print(F(", SPO2="));
+  //     // Serial.print(spo2, DEC);
+
+  //     // Serial.print(F(", SPO2Valid="));
+  //     // Serial.println(validSPO2, DEC);
+  //   }
+
+  //   //After gathering 25 new samples recalculate HR and SP02
+  //   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+    long now = millis();   //start time of this cycle
+    int err =0;
+    
+    WiFiClient c;
+    HttpClient http(c);
+
+    // Get sensor readings
+    reading = analogRead(PIEZO_IN);
+    float h = dht.readHumidity();
+    if (h >= 60) {
+      digitalWrite(DEHUMIDIFIER_OUT, HIGH);
+    } else {
+      digitalWrite(DEHUMIDIFIER_OUT, LOW);
     }
-  }
+    delay(10);
+    float t = dht.readTemperature();
 
-  float acc_X = myIMU.readFloatAccelX();
-  float acc_Y = myIMU.readFloatAccelY();
-  float acc_Z = myIMU.readFloatAccelZ();
+    long irValue = particleSensor.getIR();
+    long redValue = particleSensor.getRed();
 
-  float gyro_X = myIMU.readFloatGyroX();
-  float gyro_Y = myIMU.readFloatGyroY();
-  float gyro_Z = myIMU.readFloatGyroZ();
-
-  std::ostringstream oss;
-  oss << "/?temp=" << t << "&humid=" << h << "&piezo=" << reading << "&beatAvg=" << beatAvg << "&acc_x=" << acc_X << "&acc_y=" << acc_Y << "&acc_z=" << acc_Z << "&gyro_x=" << gyro_X << "&gyro_y=" << gyro_Y << "&gyro_z=" << gyro_Z;
-  std::string var = oss.str();
-
-  char* sensorPath = const_cast<char*>(var.c_str());
-
-  // Display readings
-  if (timer>= 40) {
-    //Send data to server
-    err = http.get(kHostname, kHttpPort, sensorPath);
-    int debug = 0;
-    if (err == 0 && debug)
+    if (checkForBeat(irValue) == true)
     {
-      Serial.println("startedRequest ok");
+      //We sensed a beat!
+      long delta = millis() - lastBeat;
+      lastBeat = millis();
 
-      err = http.responseStatusCode();
-      if (err >= 0)
+      beatsPerMinute = 60 / (delta / 1000.0);
+
+      if (beatsPerMinute < 255 && beatsPerMinute > 20)
       {
-        Serial.print("Got status code: ");
-        Serial.println(err);
+        rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
+        rateSpot %= RATE_SIZE; //Wrap variable
 
-        // Usually you'd check that the response code is 200 or a
-        // similar "success" code (200-299) before carrying on,
-        // but we'll print out whatever response we get
+        //Take average of readings
+        beatAvg = 0;
+        for (byte x = 0 ; x < RATE_SIZE ; x++)
+          beatAvg += rates[x];
+        beatAvg /= RATE_SIZE;
+      }
 
-        err = http.skipResponseHeaders();
+      // DO DA SPO2 THINGY HERE
+      int16_t IR_signal, Red_signal;
+      // bool draw_Red = true;
+      IR_signal =  pulseIR.ma_filter(pulseIR.dc_filter(irValue)) ;
+      Red_signal = pulseRed.ma_filter(pulseRed.dc_filter(redValue));
+      // long btpm = 60000/(now - lastBeat);
+      // if (btpm > 0 && btpm < 200) beatAvg = bpm.filter((int16_t)btpm);
+      // lastBeat = now; 
+      //digitalWrite(LED, HIGH); 
+      //led_on = true;
+      // compute SpO2 ratio
+      // long numerator   = (pulseRed.avgAC() * pulseIR.avgDC())/256;
+      // long denominator = (pulseRed.avgDC() * pulseIR.avgAC())/256;
+      // int RX100 = (denominator>0) ? (numerator * 100)/denominator : 999;
+      // using formula
+      //SPO2f = (10400 - RX100*17+50)/100; 
+      int redDC = pulseRed.avgDC();
+      int irDC = pulseIR.avgDC();
+      float reading1 = (float)redValue / (float)redDC;
+      float reading2 = (float)irValue / (float)irDC;
+      SPO2 = min(reading1, reading2) / max(reading1, reading2) * 98.98989;
+      Serial.print("Red: ");
+      Serial.println(reading1, 5);
+      Serial.print("IR: ");
+      Serial.println(reading2, 5);
+      
+      Serial.print("SPO2 ==> ");
+      Serial.println(SPO2, 5);
+      
+    }
+
+    float acc_X = myIMU.readFloatAccelX();
+    float acc_Y = myIMU.readFloatAccelY();
+    float acc_Z = myIMU.readFloatAccelZ();
+
+    float gyro_X = myIMU.readFloatGyroX();
+    float gyro_Y = myIMU.readFloatGyroY();
+    float gyro_Z = myIMU.readFloatGyroZ();
+
+    std::ostringstream oss;
+    oss << "/?temp=" << t << "&humid=" << h << "&piezo=" << reading << "&beatAvg=" << beatAvg << "&spo2=" << SPO2 << "&acc_x=" << acc_X << "&acc_y=" << acc_Y << "&acc_z=" << acc_Z << "&gyro_x=" << gyro_X << "&gyro_y=" << gyro_Y << "&gyro_z=" << gyro_Z;
+    std::string var = oss.str();
+
+    char* sensorPath = const_cast<char*>(var.c_str());
+
+    // Display readings
+    if (timer>= 40) {
+      //Send data to server
+      err = http.get(kHostname, kHttpPort, sensorPath);
+      int debug = 0;
+      if (err == 0 && debug)
+      {
+        Serial.println("startedRequest ok");
+
+        err = http.responseStatusCode();
         if (err >= 0)
         {
-          int bodyLen = http.contentLength();
-          // Serial.print("Content length is: ");
-          // Serial.println(bodyLen);
-          // Serial.println();
-          // Serial.println("Body returned follows:");
-        
-          // Now we've got to the body, so we can print it out
-          unsigned long timeoutStart = millis();
-          char c;
-          // Whilst we haven't timed out & haven't reached the end of the body
-          while ( (http.connected() || http.available()) &&
-                ((millis() - timeoutStart) < kNetworkTimeout) )
+          Serial.print("Got status code: ");
+          Serial.println(err);
+
+          // Usually you'd check that the response code is 200 or a
+          // similar "success" code (200-299) before carrying on,
+          // but we'll print out whatever response we get
+
+          err = http.skipResponseHeaders();
+          if (err >= 0)
           {
-              if (http.available())
-              {
-                  c = http.read();
-                  // Print out this character
-                  Serial.print(c);
-                
-                  bodyLen--;
-                  // We read something, reset the timeout counter
-                  timeoutStart = millis();
-              }
-              else
-              {
-                  // We haven't got any data, so let's pause to allow some to
-                  // arrive
-                  delay(kNetworkDelay);
-              }
+            int bodyLen = http.contentLength();
+            // Serial.print("Content length is: ");
+            // Serial.println(bodyLen);
+            // Serial.println();
+            // Serial.println("Body returned follows:");
+          
+            // Now we've got to the body, so we can print it out
+            unsigned long timeoutStart = millis();
+            char c;
+            // Whilst we haven't timed out & haven't reached the end of the body
+            while ( (http.connected() || http.available()) &&
+                  ((millis() - timeoutStart) < kNetworkTimeout) )
+            {
+                if (http.available())
+                {
+                    c = http.read();
+                    // Print out this character
+                    Serial.print(c);
+                  
+                    bodyLen--;
+                    // We read something, reset the timeout counter
+                    timeoutStart = millis();
+                }
+                else
+                {
+                    // We haven't got any data, so let's pause to allow some to
+                    // arrive
+                    delay(kNetworkDelay);
+                }
+            }
+          }
+          else
+          {
+            Serial.print("Failed to skip response headers: ");
+            Serial.println(err);
           }
         }
         else
-        {
-          Serial.print("Failed to skip response headers: ");
+        {    
+          Serial.print("Getting response failed: ");
           Serial.println(err);
         }
       }
-      else
-      {    
-        Serial.print("Getting response failed: ");
+      else if (debug)
+      {
+        Serial.print("Connect failed: ");
         Serial.println(err);
       }
-    }
-    else if (debug)
-    {
-      Serial.print("Connect failed: ");
-      Serial.println(err);
-    }
-    http.stop();
+      http.stop();
 
 
+      // Serial.print("SPO2: ");
+      // Serial.print(spo2);
+      // Serial.print(" HR: ");
+      // Serial.println(heartRate);
+      // Print info on Serial Monitor
 
-    // Print info on Serial Monitor
-    timer = 0;
-    Serial.println("\n=========================");
-
-    Serial.print("Piezoelectric Reading: ");
-    Serial.println(reading);
-
-    Serial.print('\n');
-    Serial.print("Humidity = ");
-    Serial.print(h);
-    Serial.print("%  ");
-    Serial.print("Temperature = ");
-    Serial.print(t);
-    Serial.print("°C \n");
-
-    Serial.print('\n');
-    if (irValue < 50000) {
-      Serial.println("Oximeter Reading: no finger detected");
       
-      tft.setTextDatum(BC_DATUM);
-      tft.setTextColor(TFT_RED);  tft.setTextSize(3);
-      tft.fillScreen(TFT_BLACK);
-      tft.drawString("No Pulse", (int)(tft.width()/2), (int)(tft.height()/2), 2);
-      tft.setTextDatum(TC_DATUM);
-      tft.drawString("Detected!", (int)(tft.width()/2), (int)(tft.height()/2), 2);
-    } else {
-      Serial.print("Oximeter Reading: ");
-      Serial.print("IR = ");
-      Serial.print(irValue);
-      Serial.print(", BPM = ");
-      Serial.print(beatsPerMinute);
-      Serial.print(", Avg BPM = ");
-      Serial.println(beatAvg);
+      timer = 0;
+      Serial.println("\n=========================");
+      Serial.print("SPO2: ");
+      Serial.println(SPO2);
 
-      tft.setTextDatum(BC_DATUM);
-      tft.setTextColor(TFT_RED);  tft.setTextSize(3);
-      tft.fillScreen(TFT_BLACK);
-      tft.drawNumber(beatAvg, (int)(tft.width()/2), (int)(tft.height()/2), 2);
-      tft.setTextDatum(TC_DATUM);
-      tft.drawString("Heartrate", (int)(tft.width()/2), (int)(tft.height()/2), 2);
+      Serial.print("Piezoelectric Reading: ");
+      Serial.println(reading);
+
+      Serial.print('\n');
+      Serial.print("Humidity = ");
+      Serial.print(h);
+      Serial.print("%  ");
+      Serial.print("Temperature = ");
+      Serial.print(t);
+      Serial.print("°C \n");
+
+      Serial.print('\n');
+      if (irValue < 50000) {
+        Serial.println("Oximeter Reading: no finger detected");
+        
+        tft.setTextDatum(BC_DATUM);
+        tft.setTextColor(TFT_RED);  tft.setTextSize(3);
+        tft.fillScreen(TFT_BLACK);
+        tft.drawString("No Pulse", (int)(tft.width()/2), (int)(tft.height()/2), 2);
+        tft.setTextDatum(TC_DATUM);
+        tft.drawString("Detected!", (int)(tft.width()/2), (int)(tft.height()/2), 2);
+      } else {
+        Serial.print("Oximeter Reading: ");
+        Serial.print("IR = ");
+        Serial.print(irValue);
+        Serial.print(", Red = ");
+        Serial.print(redValue);
+        Serial.print(", BPM = ");
+        Serial.print(beatsPerMinute);
+        Serial.print(", Avg BPM = ");
+        Serial.println(beatAvg);
+
+        tft.setTextDatum(BC_DATUM);
+        tft.setTextColor(TFT_RED);  tft.setTextSize(3);
+        tft.fillScreen(TFT_BLACK);
+        tft.drawNumber(beatAvg, (int)(tft.width()/2), (int)(tft.height()/2), 2);
+        tft.setTextDatum(TC_DATUM);
+        tft.setTextColor(TFT_WHITE);  tft.setTextSize(3);
+        tft.drawString("Heartrate", (int)(tft.width()/2), (int)(tft.height()/2), 2);
+      }
+
+      Serial.print("\nAccelerometer:\n");
+      Serial.print(" X = ");
+      Serial.print(acc_X, 3);
+      Serial.print(" Y = ");
+      Serial.print(acc_Y, 3);
+      Serial.print(" Z = ");
+      Serial.println(acc_Z, 3);
+
+      Serial.print("\nGyroscope:\n");
+      Serial.print(" X = ");
+      Serial.print(gyro_X, 3);
+      Serial.print(" Y = ");
+      Serial.print(gyro_Y, 3);
+      Serial.print(" Z = ");
+      Serial.println(gyro_Z, 3);
+      
+      Serial.println("=========================");
+      
+    
     }
+    timer++;
+  //}
 
-    Serial.print("\nAccelerometer:\n");
-    Serial.print(" X = ");
-    Serial.print(acc_X, 3);
-    Serial.print(" Y = ");
-    Serial.print(acc_Y, 3);
-    Serial.print(" Z = ");
-    Serial.println(acc_Z, 3);
-
-    Serial.print("\nGyroscope:\n");
-    Serial.print(" X = ");
-    Serial.print(gyro_X, 3);
-    Serial.print(" Y = ");
-    Serial.print(gyro_Y, 3);
-    Serial.print(" Z = ");
-    Serial.println(gyro_Z, 3);
-    
-    Serial.println("=========================");
-    
-   
-  }
-  timer++;
+  
 }
